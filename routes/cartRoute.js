29528e456d79bb7f4b6ee2f659dff350cdd38042
Hussein-tarek <htarek47@gmail.com>
2023-02-16 20:28:12 +0200

    cart completed

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,20 +1,22 @@
 const express = require("express");
 
 
-const { addProductToCart, getLoggedUserCart, removeSpecificCartItem } = require("../controllers/cartService");
+const { addProductToCart, getLoggedUserCart, removeSpecificCartItem, clearCart, updateCartItemQuantity } = require("../controllers/cartService");
 
 const { protect, authorize } = require("../middlewares/auth")
 
 const router = express.Router();
-router.use(protect,authorize('user'));
+router.use(protect, authorize('user'));    /// only logged in user can do this
 router
     .route("/")
     .post(
-        protect,git
+        // protect,
         //authorize('user'),
         addProductToCart
-    ).get(getLoggedUserCart)
-router.route("/:itemId").delete(removeSpecificCartItem)
+    )
+    .get(getLoggedUserCart)
+    .delete(clearCart)
 
+router.route("/:itemId").delete(removeSpecificCartItem).put(updateCartItemQuantity)
 
 module.exports = router;
